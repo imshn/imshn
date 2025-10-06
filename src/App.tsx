257f4e0e8862ec2f85035f555a036b58e34e5c9b
iamsaziya <saziyaaansari.30@gmail.com>
2025-10-06 06:37:50 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,6 +19,7 @@ const App = () => (
       <BlogProvider>
         <Toaster />
         <Sonner />
+        <Analytics />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
